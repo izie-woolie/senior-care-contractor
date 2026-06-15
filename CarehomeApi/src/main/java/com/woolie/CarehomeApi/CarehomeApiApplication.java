b8967dbff28397110307544e89f91305c54faf66
Izie Zahba <dushara.fahzad62@gmail.com>
2026-06-15 13:50:10 +0800

    setup: spring boot

---
+++ CarehomeApi/src/main/java/com/woolie/CarehomeApi/CarehomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.woolie.CarehomeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarehomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarehomeApiApplication.class, args);
+	}
+
+}
---
+++ CarehomeApi/src/test/java/com/woolie/CarehomeApi/CarehomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woolie.CarehomeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarehomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
